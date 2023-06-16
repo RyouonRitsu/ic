96778443dbfa5da263a696e09fea5022ea3b50df
kude77 <412724618@qq.com>
2023-06-16 14:21:31 +0800

    初始化维修工单MRO模块

---
+++ src/main/kotlin/com/ryouonritsu/ic/domain/dto/MRODTO.kt
@@ -0,0 +1,29 @@
+package com.ryouonritsu.ic.domain.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+/**
+ * @Author Kude
+ * @Date 2023/6/16 11:30
+ */
+@Schema(description = "MRO entity")
+data class MRODTO(
+        @Schema(description = "维修工单ID", example = "1", required = true)
+        var id: String = "",
+        @Schema(description = "报修人ID", example = "1", required = true)
+        var userId: String = "",
+        @Schema(description = "报修房间ID", example = "1", required = true)
+        var roomId: String = "",
+        @Schema(description = "问题描述", example = "1", required = true)
+        var problem: String = "",
+        @Schema(description = "期望维修时间段", example = "1", required = true)
+        var expectTime: String = "",
+        @Schema(description = "实际维修时间段", example = "1", required = true)
+        var actualTime: String = "",
+        @Schema(description = "解决方法", example = "1", required = true)
+        var resolvent: String = "",
+        @Schema(description = "实际维修时间", example = "1", required = true)
+        var maintenanceTime: String,
+        @Schema(description = "是否解决", example = "0", required = true)
+        var isSolved: Boolean,
+)
---
+++ src/main/kotlin/com/ryouonritsu/ic/entity/MRO.kt
@@ -0,0 +1,75 @@
+package com.ryouonritsu.ic.entity
+
+import com.ryouonritsu.ic.domain.dto.MRODTO
+import java.time.LocalDateTime
+import javax.persistence.*
+
+/**
+ * @Author Kude
+ * @Date 2023/6/16 10:59
+ */
+@Entity
+class MRO(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(columnDefinition = "BIGINT COMMENT '维修工单ID'", nullable = false)
+        var id: Long = 0,
+        @Column(
+                name = "user_id",
+                columnDefinition = "BIGINT DEFAULT '0' COMMENT '用户ID'",
+                nullable = false
+        )
+        var userId: Long,
+        @Column(
+                name = "room_id",
+                columnDefinition = "BIGINT DEFAULT '0' COMMENT '房间ID'",
+                nullable = false
+        )
+        var roomId: Long,
+        @Column(columnDefinition = "LONGTEXT DEFAULT '' COMMENT '问题描述'", nullable = false)
+        var problem: String = "",
+        @Column(columnDefinition = "VARCHAR(255) DEFAULT '' COMMENT '期望时间段'", nullable = false)
+        var expectTime: String = "",
+        @Column(columnDefinition = "VARCHAR(255) DEFAULT '' COMMENT '实际时间段'", nullable = false)
+        var actualTime: String = "",
+        @Column(columnDefinition = "LONGTEXT DEFAULT '' COMMENT '问题解决方法'", nullable = false)
+        var resolvent: String = "",
+        @Column(columnDefinition = "VARCHAR(255) DEFAULT '' COMMENT '维修时间'", nullable = false)
+        var maintenanceTime: String = "",
+        @Column(
+                name = "is_solved",
+                columnDefinition = "TINYINT(3) DEFAULT '0' COMMENT '是否解决'",
+                nullable = false
+        )
+        var isSolved: Boolean = false,
+        @Column(
+                name = "is_deleted",
+                columnDefinition = "TINYINT(3) DEFAULT '0' COMMENT '是否已删除'",
+                nullable = false
+        )
+        var isDeleted: Boolean = false,
+        @Column(
+                name = "create_time",
+                columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'",
+                nullable = false
+        )
+        var createTime: LocalDateTime = LocalDateTime.now(),
+        @Column(
+                name = "modify_time",
+                columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'",
+                nullable = false
+        )
+        var modifyTime: LocalDateTime = LocalDateTime.now(),
+) {
+    fun toDTO() = MRODTO(
+            id = "$id",
+            userId = "$userId",
+            roomId = "$roomId",
+            problem = problem,
+            expectTime = expectTime,
+            actualTime = actualTime,
+            resolvent = resolvent,
+            maintenanceTime = maintenanceTime,
+            isSolved = isSolved,
+    )
+}
