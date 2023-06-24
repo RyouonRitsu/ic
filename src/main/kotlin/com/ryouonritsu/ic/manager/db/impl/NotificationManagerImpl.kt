3a2abbe6ee56b29995fb2ddc3ab4765e2a7733d3
zhouhongxi <zhouhongxi@bytedance.com>
2023-06-24 14:36:44 +0800

    [feature-event] 通知服务发送邮件

--- src/main/kotlin/com/ryouonritsu/ic/manager/db/impl/NotificationManagerImpl.kt
+++ src/main/kotlin/com/ryouonritsu/ic/manager/db/impl/NotificationManagerImpl.kt
@@ -1,11 +1,17 @@
 package com.ryouonritsu.ic.manager.db.impl
 
 import com.ryouonritsu.ic.common.annotation.ServiceLog
+import com.ryouonritsu.ic.common.constants.ICConstant
+import com.ryouonritsu.ic.common.enums.ExceptionEnum
+import com.ryouonritsu.ic.common.exception.ServiceException
 import com.ryouonritsu.ic.domain.protocol.request.PublishRequest
 import com.ryouonritsu.ic.entity.Event
 import com.ryouonritsu.ic.manager.db.NotificationManager
+import com.ryouonritsu.ic.manager.db.UserManager
 import com.ryouonritsu.ic.repository.EventRepository
+import com.ryouonritsu.ic.repository.UserRepository
 import org.slf4j.LoggerFactory
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.stereotype.Component
 import org.springframework.transaction.support.TransactionTemplate
 
@@ -15,7 +21,10 @@ import org.springframework.transaction.support.TransactionTemplate
 @Component
 class NotificationManagerImpl(
     private val eventRepository: EventRepository,
-    private val transactionTemplate: TransactionTemplate
+    private val userManager: UserManager,
+    private val userRepository: UserRepository,
+    private val transactionTemplate: TransactionTemplate,
+    private val asyncTaskExecutor: ThreadPoolTaskExecutor
 ) : NotificationManager {
     companion object {
         private val log = LoggerFactory.getLogger(NotificationManagerImpl::class.java)
@@ -27,6 +36,7 @@ class NotificationManagerImpl(
         transactionTemplate.execute {
             event = eventRepository.save(event)
         }
+        asyncSendNotification(listOf(request))
 
         return event
     }
@@ -37,6 +47,7 @@ class NotificationManagerImpl(
         transactionTemplate.execute {
             events = eventRepository.saveAll(events)
         }
+        asyncSendNotification(requests)
 
         return events
     }
@@ -45,5 +56,41 @@ class NotificationManagerImpl(
     override fun batchPublish(userIds: List<Long>, name: String?, message: String): List<Event> {
         val requests = userIds.map { PublishRequest(it, name, message) }
         return batchPublish(requests)
+    }
+
+    private fun asyncSendNotification(requests: List<PublishRequest>) {
+        asyncTaskExecutor.submit {
+            if (requests.isEmpty()) return@submit
+
+            val userIds = requests.map { it.userId!! }
+            val users = userRepository.findAllById(userIds)
+                .associateBy { it.id }
+            if (users.isEmpty()) {
+                log.warn("[NotificationManagerImpl.asyncSendNotification] can not find $userIds in database")
+                return@submit
+            }
+
+            requests.forEach {
+                val f = asyncTaskExecutor.submitListenable {
+                    val user = users[it.userId]
+                        ?: throw ServiceException(ExceptionEnum.OBJECT_DOES_NOT_EXIST)
+                    val result = userManager.retrySendEmail(
+                        user.email,
+                        it.name ?: ICConstant.EVENT,
+                        it.message!!
+                    )
+                    if (!result) throw ServiceException(ExceptionEnum.REQUEST_TIMEOUT)
+                }
+                f.addCallback(
+                    { log.info("[NotificationManagerImpl.asyncSendNotification] send notification success!") },
+                    {
+                        log.error(
+                            "[NotificationManagerImpl.asyncSendNotification] send notification failed!",
+                            it
+                        )
+                    }
+                )
+            }
+        }
     }
 }
